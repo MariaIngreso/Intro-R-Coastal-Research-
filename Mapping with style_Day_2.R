1d5a21e9191392785f6dd44a9fc6d452167e6357
MariaIngreso <kristina.ingreso@gmail.com>
2019-04-11 10:58:34 +0200

    updated mapping_with_style

--- Mapping with style_Day_2.R
+++ Mapping with style_Day_2.R
@@ -18,3 +18,25 @@ sa_1 <- ggplot() +
   coord_equal(xlim = c(12, 36), ylim = c(-38, -22), expand = 0)           # Force lon/lat extent
 sa_1
 
+
+sa_2 <- sa_1 +
+  annotate("text", label = "Atlantic\nOcean",                             # "n" starts a new line 
+           x = 15.1, y = -32.0,
+           size = 5.0,
+           angle = 30,                                                    # angle in degrees : 0 t0 360 
+           colour = "navy") +
+  annotate("text", label = "Indian\nOcean",
+           x = 33.2, y = -34.2,
+           size = 5.0,
+           angle = 330,
+           colour = "springgreen")
+sa_2
+
+sa_3 <- sa_2 +
+  scalebar(x.min = 22, x.max = 26, y.min = -36, y.max = -35, # Set location of bar
+           dist = 200, height = 1, st.dist = 0.8, st.size = 4, # Set particulars
+           dd2km = TRUE, model = "WGS84") + # Set appearance
+  north(x.min = 22.5, x.max = 25.5, y.min = -33, y.max = -31, # Set location of symbol
+        scale = 1.2, symbol = 16)
+sa_3
+
